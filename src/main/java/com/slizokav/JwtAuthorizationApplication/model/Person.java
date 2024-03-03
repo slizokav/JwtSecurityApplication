ca0792eeb4fc9e8c3b2931c6e79d79e9d461f462
Дмитрий Слизовский <slizokav@gmail.com>
2024-03-03 16:29:36 +0300

    initialize basic repository

---
+++ src/main/java/com/slizokav/JwtAuthorizationApplication/model/Person.java
@@ -0,0 +1,68 @@
+package com.slizokav.JwtAuthorizationApplication.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "person")
+public class Person {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "age")
+    private int age;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Person(String username, String password, int age) {
+        this.username = username;
+        this.password = password;
+        this.age = age;
+    }
+
+    public Person(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public Person() {
+    }
+}
---
+++ src/test/java/com/slizokav/JwtAuthorizationApplication/JwtAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slizokav.JwtAuthorizationApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
